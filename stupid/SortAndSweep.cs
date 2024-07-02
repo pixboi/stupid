3158fda0f824f96a50cfae5c3905b79a9bb1bcad
topitsky <topitsky@gmail.com>
2024-07-02 16:23:39 +0300

    tttt

--- stupid/SortAndSweep.cs
+++ stupid/SortAndSweep.cs
@@ -11,33 +11,23 @@ namespace stupid
     public class SortAndSweepBroadphase : IBroadphase
     {
         private readonly List<AxisEndpoint> endpointsX;
-        private readonly List<AxisEndpoint> endpointsY;
-        private readonly List<AxisEndpoint> endpointsZ;
         private readonly HashSet<BodyPair> pairs;
         private int rbCount = 0;
 
         public SortAndSweepBroadphase(int initialCapacity = 100)
         {
             endpointsX = new List<AxisEndpoint>(initialCapacity * 2);
-            //  endpointsY = new List<AxisEndpoint>(initialCapacity * 2);
-            //  endpointsZ = new List<AxisEndpoint>(initialCapacity * 2);
             pairs = new HashSet<BodyPair>(initialCapacity * 2);
         }
 
         private void Rebuild(List<Rigidbody> rigidbodies)
         {
             endpointsX.Clear();
-            //  endpointsY.Clear();
-            //  endpointsZ.Clear();
             foreach (var body in rigidbodies)
             {
                 var bounds = body.collider.GetBounds();
                 endpointsX.Add(new AxisEndpoint { Value = bounds.Min.x, IsMin = true, Body = body });
                 endpointsX.Add(new AxisEndpoint { Value = bounds.Max.x, IsMin = false, Body = body });
-                //  endpointsY.Add(new AxisEndpoint { Value = bounds.Min.y, IsMin = true, Body = body });
-                // endpointsY.Add(new AxisEndpoint { Value = bounds.Max.y, IsMin = false, Body = body });
-                // endpointsZ.Add(new AxisEndpoint { Value = bounds.Min.z, IsMin = true, Body = body });
-                // endpointsZ.Add(new AxisEndpoint { Value = bounds.Max.z, IsMin = false, Body = body });
             }
             rbCount = rigidbodies.Count;
         }
@@ -51,40 +41,21 @@ namespace stupid
 
             pairs.Clear();
 
-            UpdateEndpoints(endpointsX, 0);
-            // UpdateEndpoints(endpointsY, 1);
-            // UpdateEndpoints(endpointsZ, 2);
-
+            UpdateEndpoints(endpointsX);
             InsertionSort(endpointsX);
-            // InsertionSort(endpointsY);
-            // InsertionSort(endpointsZ);
-            //
             SweepAndPrune(endpointsX);
-            // SweepAndPrune(endpointsY);
-            // SweepAndPrune(endpointsZ);
 
             return pairs;
         }
 
-        private void UpdateEndpoints(List<AxisEndpoint> endpoints, int axis)
+        private void UpdateEndpoints(List<AxisEndpoint> endpoints)
         {
             foreach (var endpoint in endpoints)
             {
                 if (endpoint.Body.isSleeping) continue;
 
                 var bounds = endpoint.Body.collider.GetBounds();
-                switch (axis)
-                {
-                    case 0:
-                        endpoint.Value = endpoint.IsMin ? bounds.Min.x : bounds.Max.x;
-                        break;
-                    case 1:
-                        endpoint.Value = endpoint.IsMin ? bounds.Min.y : bounds.Max.y;
-                        break;
-                    case 2:
-                        endpoint.Value = endpoint.IsMin ? bounds.Min.z : bounds.Max.z;
-                        break;
-                }
+                endpoint.Value = endpoint.IsMin ? bounds.Min.x : bounds.Max.x;
             }
         }
 
