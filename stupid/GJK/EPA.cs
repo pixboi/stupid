a1c195267c3b7040484ef2e73e21772d3bcf21f4
topitsky <topitsky@gmail.com>
2024-08-02 12:38:55 +0300

    b

---
+++ stupid/GJK/EPA.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using stupid.Maths;
+
+namespace stupid.GJK
+{
+    public class EPA
+    {
+        private struct Edge
+        {
+            public Vector3S A;
+            public Vector3S B;
+            public Vector3S Normal;
+            public f32 Distance;
+        }
+
+        public Vector3S FindPenetrationDepth(ISupport shapeA, ISupport shapeB, GJK.Simplex simplex)
+        {
+            List<Vector3S> polytope = simplex.Points;
+            List<Edge> edges = new List<Edge>();
+
+            while (true)
+            {
+                int closestEdgeIndex = -1;
+                f32 minDistance = f32.maxValue;
+
+                for (int i = 0; i < polytope.Count; i++)
+                {
+                    Vector3S A = polytope[i];
+                    Vector3S B = polytope[(i + 1) % polytope.Count];
+                    Vector3S edgeNormal = Vector3S.Cross(B - A, polytope[0] - A).Normalize();
+                    f32 distance = Vector3S.Dot(edgeNormal, A);
+
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestEdgeIndex = i;
+                    }
+
+                    edges.Add(new Edge { A = A, B = B, Normal = edgeNormal, Distance = distance });
+                }
+
+                Edge closestEdge = edges[closestEdgeIndex];
+                Vector3S newPoint = Support(shapeA, shapeB, closestEdge.Normal);
+
+                f32 distanceToNewPoint = Vector3S.Dot(closestEdge.Normal, newPoint);
+
+                if (MathS.Abs(distanceToNewPoint - closestEdge.Distance) < f32.epsilon)
+                {
+                    return closestEdge.Normal * distanceToNewPoint;
+                }
+
+                polytope.Insert(closestEdgeIndex + 1, newPoint);
+                edges.Clear();
+            }
+        }
+
+        private Vector3S Support(ISupport shapeA, ISupport shapeB, Vector3S direction)
+        {
+            Vector3S pointA = shapeA.Support(direction);
+            Vector3S pointB = shapeB.Support(-direction);
+            return pointA - pointB;
+        }
+    }
+
+}
---
+++ stupid/GJK/GJK.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using stupid.Maths;
+namespace stupid.GJK
+{
+    public interface ISupport
+    {
+        Vector3S Support(Vector3S direction);
+    }
+
+
+    public class GJK
+    {
+        public struct Simplex
+        {
+            public List<Vector3S> Points;
+
+            public Simplex(Vector3S a)
+            {
+                Points = new List<Vector3S> { a };
+            }
+
+            public void Add(Vector3S point)
+            {
+                Points.Insert(0, point);
+            }
+
+            public void RemoveLast()
+            {
+                Points.RemoveAt(Points.Count - 1);
+            }
+
+            public Vector3S Last()
+            {
+                return Points[Points.Count - 1];
+            }
+        }
+
+        public bool Intersect(ISupport shapeA, ISupport shapeB)
+        {
+            Vector3S direction = new Vector3S(1, 0, 0); // Arbitrary initial direction
+            Simplex simplex = new Simplex(Support(shapeA, shapeB, direction));
+
+            direction = -simplex.Last();
+
+            while (true)
+            {
+                Vector3S A = Support(shapeA, shapeB, direction);
+                if (Vector3S.Dot(A, direction) <= f32.zero)
+                {
+                    return false;
+                }
+
+                simplex.Add(A);
+
+                if (SimplexContainsOrigin(ref simplex, ref direction))
+                {
+                    return true;
+                }
+            }
+        }
+
+        private Vector3S Support(ISupport shapeA, ISupport shapeB, Vector3S direction)
+        {
+            Vector3S pointA = shapeA.Support(direction);
+            Vector3S pointB = shapeB.Support(-direction);
+            return pointA - pointB;
+        }
+
+        private bool SimplexContainsOrigin(ref Simplex simplex, ref Vector3S direction)
+        {
+            Vector3S A = simplex.Points[0];
+            Vector3S AO = -A;
+
+            if (simplex.Points.Count == 3)
+            {
+                Vector3S B = simplex.Points[1];
+                Vector3S C = simplex.Points[2];
+
+                Vector3S AB = B - A;
+                Vector3S AC = C - A;
+
+                Vector3S ABPerp = Vector3S.Cross(Vector3S.Cross(AC, AB), AB);
+                Vector3S ACPerp = Vector3S.Cross(Vector3S.Cross(AB, AC), AC);
+
+                if (Vector3S.Dot(ABPerp, AO) > f32.zero)
+                {
+                    simplex.RemoveLast();
+                    direction = ABPerp;
+                }
+                else if (Vector3S.Dot(ACPerp, AO) > f32.zero)
+                {
+                    simplex.Points.RemoveAt(1);
+                    direction = ACPerp;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                Vector3S B = simplex.Points[1];
+                Vector3S AB = B - A;
+                direction = Vector3S.Cross(Vector3S.Cross(AB, AO), AB);
+            }
+
+            return false;
+        }
+    }
+
+}
