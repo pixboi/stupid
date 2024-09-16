66d9656b02afaab89487aab1227aa751afff36a4
topitsky <topitsky@gmail.com>
2024-09-16 17:46:07 +0300

    ffffff more tight substeps seem infinitely better than iterations

--- stupid/Colliders/BoxMath.cs
+++ stupid/Colliders/BoxMath.cs
@@ -77,7 +77,7 @@ namespace stupid.Colliders
                         var feature = p.Item2;
                         var pen = minPen;
 
-                        //if (a.RayTest(vertex, normalV, minPen, out var pointInBox, out var distance)) pen = distance;
+                        // if (a.RayTest(vertex, normalV, minPen, out var pointInBox, out var distance)) pen = distance;
 
                         contacts[count++] = new ContactS(vertex, normalV, -pen, a.collidable, b.collidable, feature);
                     }
