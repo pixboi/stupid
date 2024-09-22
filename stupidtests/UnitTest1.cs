8f1af569d37265c8aaf12e55208f2e401147ae3f
topitsky <topitsky@gmail.com>
2024-09-22 19:35:54 +0300

    asd

--- stupidtests/UnitTest1.cs
+++ stupidtests/UnitTest1.cs
@@ -6,7 +6,8 @@ namespace stupidtests
     [TestClass]
     public class UnitTest1
     {
-        public int iterations = 100000000;
+        public int iterations = 10000000;
+
         [TestMethod]
         public void f32Test()
         {
@@ -18,11 +19,51 @@ namespace stupidtests
             }
 
             var temp = new f32();
-           
+
             for (int i = 0; i < iterations; i++)
             {
                 temp += list[i];
-                temp *= 
+
+                list[i] = temp;
+            }
+        }
+
+        [TestMethod]
+        public void LongTest()
+        {
+            var list = new long[iterations];
+
+            for (int i = 0; i < iterations; i++)
+            {
+                list[i] = i;
+            }
+
+            var temp = 0L;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                temp += list[i];
+
+                list[i] = temp;
+            }
+        }
+
+        [TestMethod]
+        public void IntTest()
+        {
+            var list = new int[iterations];
+
+            for (int i = 0; i < iterations; i++)
+            {
+                list[i] = i;
+            }
+
+            var temp = 0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                temp += list[i];
+
                 list[i] = temp;
             }
         }
