d9c7dd27ff25332e658bf7dea45dbb247f66936d
topitsky <topitsky@gmail.com>
2024-12-18 11:10:52 +0200

    w

--- stupid/Constraints/ContactSlim.cs
+++ stupid/Constraints/ContactSlim.cs
@@ -8,20 +8,20 @@ namespace stupid.Constraints
     public struct ContactSlim
     {
         public readonly Vector3S point; //24
-        public Vector3S tangent; // 24
+        public Vector3S tangent; //24
         public f32 normalMass, accumulatedImpulse; // 16
-        public f32 tangentMass, accumulatedFriction;// 16
-        public readonly sbyte featureId; //1
+        public f32 tangentMass, accumulatedFriction; // 16
+        public readonly int featureId; //1
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ContactSlim(Collidable a, Collidable b, in ContactData data)
         {
-            featureId = (sbyte)data.featureId;
+            featureId = data.featureId;
             accumulatedImpulse = f32.zero;
             normalMass = f32.zero;
+            accumulatedFriction = f32.zero;
             tangentMass = f32.zero;
             tangent = Vector3S.zero;
-            accumulatedFriction = f32.zero;
             point = data.point;
         }
 
@@ -31,6 +31,48 @@ namespace stupid.Constraints
             var av = a.velocity + Vector3S.Cross(a.angularVelocity, ra);
             var bv = b.isDynamic ? b.velocity + Vector3S.Cross(b.angularVelocity, rb) : Vector3S.zero;
             return bv - av;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3S CalculateTangent(in Vector3S normal, in Vector3S contactVelocity)
+        {
+            // Calculate relative velocity at the contact point
+            Vector3S normalVelocity = normal * Vector3S.Dot(contactVelocity, normal);
+            Vector3S tangentialVelocity = contactVelocity - normalVelocity;
+            return tangentialVelocity.Normalize();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static f32 CalculateTangentMass(in RigidbodyData a, in RigidbodyData b, in Vector3S ra, in Vector3S rb, in Vector3S tangent)
+        {
+            // Precompute cross products for mass calculation
+            var raCrossTangent = Vector3S.Cross(ra, tangent);
+            var tangentMass = a.inverseMass + Vector3S.Dot(Vector3S.Cross(a.inertiaWorld * raCrossTangent, ra), tangent);
+
+            if (b.isDynamic)
+            {
+                var rbCrossTangent = Vector3S.Cross(rb, tangent);
+                tangentMass += b.inverseMass + Vector3S.Dot(Vector3S.Cross(b.inertiaWorld * rbCrossTangent, rb), tangent);
+            }
+
+            return tangentMass > f32.zero ? -(f32.one / tangentMass) : f32.zero;
+        }
+
+        public static f32 CalculateNormalMass(in RigidbodyData a, in RigidbodyData b, in Vector3S ra, in Vector3S rb, in Vector3S normal)
+        {
+            //I think this is fine, since normal or inertia doesnt change between frames
+            Vector3S raCrossNormal = Vector3S.Cross(ra, normal);
+            f32 angularMassA = Vector3S.Dot(raCrossNormal, a.inertiaWorld * raCrossNormal);
+            f32 effectiveMass = a.inverseMass + angularMassA;
+
+            if (b.isDynamic)
+            {
+                Vector3S rbCrossNormal = Vector3S.Cross(rb, normal);
+                f32 angularMassB = Vector3S.Dot(rbCrossNormal, b.inertiaWorld * rbCrossNormal);
+                effectiveMass += b.inverseMass + angularMassB;
+            }
+
+            return effectiveMass > f32.zero ? -(f32.one / effectiveMass) : f32.zero;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -52,55 +94,20 @@ namespace stupid.Constraints
             var ra = this.point - a.position;
             var rb = this.point - b.position;
 
-            //I think this is fine, since normal or inertia doesnt change between frames
-            Vector3S raCrossNormal = Vector3S.Cross(ra, manifold.normal);
-            f32 angularMassA = Vector3S.Dot(raCrossNormal, a.inertiaWorld * raCrossNormal);
-            f32 effectiveMass = a.inverseMass + angularMassA;
+            this.normalMass = CalculateNormalMass(a, b, ra, rb, manifold.normal);
 
-            if (b.isDynamic)
-            {
-                Vector3S rbCrossNormal = Vector3S.Cross(rb, manifold.normal);
-                f32 angularMassB = Vector3S.Dot(rbCrossNormal, b.inertiaWorld * rbCrossNormal);
-                effectiveMass += b.inverseMass + angularMassB;
-            }
-
-            this.normalMass = effectiveMass > f32.zero ? -(f32.one / effectiveMass) : f32.zero;
-
-            /*
-             *             //However, now that we apply a gravity each time in the correct order (first), previuosly after contacst, we shoudl always have a proper tangent
-            //In retain, the previous tangent is stored IN THIS.TANGENT!
-            f32 tangentMag = tangentialVelocity.sqrMagnitude;
-            var oldTangent = this.tangent;
-            var newTangent = tangentialVelocity.Normalize();
-            var blend = MathS.Clamp(tangentMag, f32.zero, f32.small) / f32.small;
-            this.tangent = Vector3S.Lerp(oldTangent, newTangent, blend).Normalize();
-            */
-
-            // Calculate relative velocity at the contact point
             var contactVelocity = CalculateContactVelocity(a, b, ra, rb);
-            Vector3S normalVelocity = manifold.normal * Vector3S.Dot(contactVelocity, manifold.normal);
-            Vector3S tangentialVelocity = contactVelocity - normalVelocity;
-            this.tangent = tangentialVelocity.Normalize();
-
-            // Precompute cross products for mass calculation
-            var raCrossTangent = Vector3S.Cross(ra, tangent);
-            var tMass = a.inverseMass + Vector3S.Dot(Vector3S.Cross(a.inertiaWorld * raCrossTangent, ra), tangent);
-
-            if (b.isDynamic)
-            {
-                var rbCrossTangent = Vector3S.Cross(rb, tangent);
-                tMass += b.inverseMass + Vector3S.Dot(Vector3S.Cross(b.inertiaWorld * rbCrossTangent, rb), tangent);
-            }
-
-            this.tangentMass = tMass > f32.zero ? f32.one / tMass : f32.zero;
-            this.tangentMass = -this.tangentMass;
+            this.tangent = CalculateTangent(manifold.normal, contactVelocity);
+            this.tangentMass = CalculateTangentMass(a, b, ra, rb, tangent);
         }
+
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void WarmStart(ref RigidbodyData a, ref RigidbodyData b, in Vector3S normal)
         {
             var ra = this.point - a.position;
             var rb = this.point - b.position;
+
             Vector3S warmImpulse = (normal * accumulatedImpulse) + (tangent * accumulatedFriction);
             ApplyImpulse(ref a, ref b, warmImpulse, ra, rb);
         }
@@ -110,35 +117,34 @@ namespace stupid.Constraints
         {
             var ra = this.point - a.position;
             var rb = this.point - b.position;
-
-            // Precompute velocities
             var contactVelocity = CalculateContactVelocity(a, b, ra, rb);
+
             var relativeNormalVelocity = Vector3S.Dot(contactVelocity, normal);
 
-            var normalImpulse = this.normalMass * (relativeNormalVelocity + bias);
-            var oldAccumulatedImpulse = this.accumulatedImpulse;
-            this.accumulatedImpulse = MathS.Max(oldAccumulatedImpulse + normalImpulse, f32.zero);
-            normalImpulse = this.accumulatedImpulse - oldAccumulatedImpulse;
+            var impulse = this.normalMass * (relativeNormalVelocity + bias);
+            var oldImpulse = this.accumulatedImpulse;
+            this.accumulatedImpulse = MathS.Max(oldImpulse + impulse, f32.zero);
+            impulse = this.accumulatedImpulse - oldImpulse;
 
-            var totalImpulse = (normal * normalImpulse);
-            ApplyImpulse(ref a, ref b, totalImpulse, ra, rb);
+            var added = (normal * impulse);
+            ApplyImpulse(ref a, ref b, added, ra, rb);
         }
 
         public void SolveFriction(ref RigidbodyData a, ref RigidbodyData b, in Vector3S normal, in f32 bias, in f32 friction)
         {
             var ra = this.point - a.position;
             var rb = this.point - b.position;
-
             var contactVelocity = CalculateContactVelocity(a, b, ra, rb);
 
-            var fric = this.tangentMass * Vector3S.Dot(contactVelocity, this.tangent);
-            var maxFric = this.accumulatedImpulse * friction;
-            var oldAccumulatedFriction = this.accumulatedFriction;
-            this.accumulatedFriction = MathS.Clamp(oldAccumulatedFriction + fric, -maxFric, maxFric);
-            fric = this.accumulatedFriction - oldAccumulatedFriction;
+            var couloumbMax = this.accumulatedImpulse * friction;
 
-            var totalImpulse = (this.tangent * fric);
-            ApplyImpulse(ref a, ref b, totalImpulse, ra, rb);
+            var impulse = tangentMass * Vector3S.Dot(contactVelocity, tangent);
+            var oldImpulse = this.accumulatedFriction;
+            this.accumulatedFriction = MathS.Clamp(oldImpulse + impulse, -couloumbMax, couloumbMax);
+            impulse = this.accumulatedFriction - oldImpulse;
+
+            var added = (tangent * impulse);
+            ApplyImpulse(ref a, ref b, added, ra, rb);
         }
     }
 }
